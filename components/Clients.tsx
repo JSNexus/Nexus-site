e16e1ce444ff61455219e4ae67445ff7dce3b9fc
JacintoSergio <60ssegundo@gmail.com>
2024-11-27 11:20:52 +0200

    Alteracao nos arquivos clients e experience de img para image

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Image from "next/image";
 
 import React from "react";
 
@@ -29,15 +30,18 @@ const Clients = () => {
           {companies.map((company) => (
             <React.Fragment key={company.id}>
               <div className="flex md:max-w-60 max-w-32 gap-2">
-                <img
+                <Image
                   src={company.img}
                   alt={company.name}
+                  width={company.id === 4 || company.id === 5 ? 40 : 20}
+                  height={company.id === 4 || company.id === 5 ? 40 : 20}
                   className="md:w-10 w-5"
                 />
-                <img
+                <Image
                   src={company.nameImg}
                   alt={company.name}
                   width={company.id === 4 || company.id === 5 ? 100 : 150}
+                  height={30}
                   className="md:w-24 w-20"
                 />
               </div>
